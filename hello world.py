058075a7daf69f79bccab8efb85e5ae442d951b0
Garry Wong <gwong@vendasta.com>
2014-03-05 13:05:28 -0600

    New lines agan

--- hello world.py
+++ hello world.py
@@ -7,3 +7,4 @@ print  "But this is being added on develop branch"
 """ so i'm going to add a couple of lines of comments. okay how about this? is this less ridiculous?
 and this should be in the feature branch. """
 
+print "Hello, I am writing this in new lines 2"
