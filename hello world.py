998db57ba50868b4873b22286c4400e76748ab9c
Garry Wong <gwong@vendasta.com>
2014-03-05 12:56:24 -0600

    Addressing PR feedback

--- hello world.py
+++ hello world.py
@@ -5,5 +5,5 @@ print  "HELLO WORLD This should be master"
 print  "But this is being added on develop branch"
 
 """ so i'm going to add a couple of lines of comments
-and this should be in the feature branch"""
+and this should be in the feature branch. okay how about this? is this less ridiculous?"""
 
